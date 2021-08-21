354a0656595a446378162f59fa9b628073127a00
Dima <strebkov003@bk.ru>
2021-08-21 19:15:45 +0300

    added dynamically changing records

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import AppHeader from '../app-header/app-header';
-import SearchPanel from '../search-panel/search-panel';
-import PostStatusFilter from '../post-status-filter/post-status-filter';
-import PostList from '../post-list/post-list';
-import PostAddForm from '../post-add-form/post-add-form';
-import './app.css'
+import AppHeader from "../app-header/app-header";
+import SearchPanel from "../search-panel/search-panel";
+import PostStatusFilter from "../post-status-filter/post-status-filter";
+import PostList from "../post-list/post-list";
+import PostAddForm from "../post-add-form/post-add-form";
+import "./app.css";
 
 const App = () => {
-    return (
-       <div className="app">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
-            </div>
-            <PostList/>
-            <PostAddForm/>
-       </div>
-    )
-}
+  const data = [
+    { label: "ti lox", key: "sfddfgfd" },
+    { label: "ti l2ox", key: "sfdgdfg" },
+    { label: "ti l5x", key: "sfdcvb" },
+  ];
+
+  return (
+    <div className="app">
+      <AppHeader />
+      <div className="search-panel d-flex">
+        <SearchPanel />
+        <PostStatusFilter />
+      </div>
+      <PostList data={data} />
+      <PostAddForm />
+    </div>
+  );
+};
 
 export default App;
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import PostListItem from '../post-list-item/post-list-item';
-import './post-list.css';
+import PostListItem from "../post-list-item/post-list-item";
+import "./post-list.css";
 
-const PostList = () => {
-    return (
-        <ul className = "app-list list-group">
-            <PostListItem />
-            <PostListItem />
-            <PostListItem />
-        </ul>
-    )
-}
+const PostList = ({ data }) => {
+  const elem = data.map((item) => {
+    if (typeof item === "object" && isEmpty(item)) {
+      const { id, ...ItemPost } = item;
+      return (
+        <li key={id} className="list-group-item">
+          <PostListItem {...ItemPost} />
+        </li>
+      );
+    }
+    return null;
+  });
+
+  function isEmpty(obj) {
+    for (const key in obj) {
+      return true;
+    }
+    return false;
+  }
+
+  return <ul className="app-list list-group">{elem}</ul>;
+};
 
 export default PostList;
